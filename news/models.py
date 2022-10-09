4d88fe5a2d3c3da679aebef0cb561054f912143a
blukky <ya.gud@mail.ru>
2022-10-09 08:46:30 +0300

    FINAL

--- news/models.py
+++ news/models.py
@@ -9,6 +9,7 @@ class Media(models.Model):
     url = models.URLField(max_length=255, verbose_name="Ссылка на СМИ")
     logo = models.URLField(verbose_name="Ссылка на логотип СМИ")
     rss_url = models.URLField(verbose_name="RSS поток")
+    inoagent = models.BooleanField(default=False, verbose_name="Иноагент")
 
     class Meta:
         verbose_name = "СМИ"
@@ -45,11 +46,11 @@ class Artilce(models.Model):
     img = models.URLField(verbose_name="Фотография статьи", null=True, blank=True)
     description = models.CharField(max_length=1500, verbose_name="Краткое описание статьи", null=True, blank=True)
     body = models.TextField(verbose_name="Текст статьи", null=True, blank=True)
-    tags = models.ManyToManyField(Tags, verbose_name="Теги")
+    tags = models.ManyToManyField(Tags, verbose_name="Теги", null=True, blank=True)
     media = models.ForeignKey(Media, on_delete=models.SET_NULL, null=True, verbose_name="СМИ")
     category = models.IntegerField(verbose_name="Какой роли относится статья", null=True, blank=True)
     date = models.DateTimeField(auto_now=False, verbose_name="Дата публикации", null=True, blank=True)
-
+    link = models.URLField(verbose_name="Ссылка на статью", blank=True, null=True)
 
     class Meta:
         verbose_name = "Статья"
--- news_parser.py
+++ news_parser.py
@@ -9,5 +9,5 @@ import feedparser
 # print(newsapi.get_sources(language="ru"))
 agent = "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Mobile Safari/537.36"
 
-FeedPaper = feedparser.parse("http://redirect.subscribe.ru/www.kommersant.ru/RSS/corp.xml", agent=agent)
-print(FeedPaper.entries)
+FeedPaper = feedparser.parse("https://russian.rt.com/rss", agent=agent)
+print(FeedPaper.entries[0])
