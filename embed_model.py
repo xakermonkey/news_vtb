c8e1c15871549072e2d96f2884fe2d19ed1c7edf
blukky <ya.gud@mail.ru>
2022-10-09 03:12:16 +0300

    models

--- embed_model.py
+++ embed_model.py
@@ -1,1 +1,21 @@
+from allennlp.modules.token_embedders import ElmoTokenEmbedder
+from allennlp.data.token_indexers.elmo_indexer import ELMoTokenCharactersIndexer
+from allennlp.data.tokenizers.token_class import Token
+import torch
+import numpy as np
+from allennlp.data.vocabulary import Vocabulary
 
+sentence = "Привет мир"
+
+tokens = [Token(word) for word in sentence.split()]
+
+
+elmo_embedder = ElmoTokenEmbedder(options_file="196/options.json", weight_file="196/model.hdf5")
+indexer = ELMoTokenCharactersIndexer()
+vocab = Vocabulary()
+vocab.set_from_file("196/tokens.txt", is_padded=False)
+character_indices = indexer.tokens_to_indices(tokens, vocab)["elmo_tokens"]
+
+indices_tensor = torch.LongTensor([character_indices])
+
+embeddings = elmo_embedder(indices_tensor)[0]
--- news/views.py
+++ news/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from .models import *
+import feedparser
+
+agent = "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Mobile Safari/537.36"
+
 
 def index(request):
+    n = []
+    for media in Media.objects.all():
+        feedpaper = feedparser.parse(media.rss_url, agent=agent)
+        for new in feedpaper.entries:
+            n.append(new)
+
     return render(request, 'index.html')
 
 
